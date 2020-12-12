6e665627a7df1325701b14a8dac375d954973f87
Rogier Spieker <rogier@konfirm.net>
2020-12-12 10:20:32 +0100

    ES6 syntax
    As UI5 is just javascript, write it how you want it (browser support allowing)

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,15 @@
 sap.ui.define([
 	'sap/ui/core/mvc/Controller',
 	'sap/m/MessageToast'
-], function (Controller, MessageToast) {
+], (Controller, MessageToast) => {
 	'use strict';
 
-	return Controller.extend('sap.ui.demo.walkthrough.controller.App', {
-		onShowHello: function () {
-			MessageToast.show('Hello World');
+	return Controller.extend(
+		'sap.ui.demo.walkthrough.controller.App',
+		{
+			onShowHello() {
+				MessageToast.show('Hello World');
+			}
 		}
-	});
+	);
 });
