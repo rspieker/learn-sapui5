355490e71dd78f8348b7734b06cf01308de67fea
Rogier Spieker <rogier@konfirm.net>
2020-12-12 14:04:03 +0100

    cleanup that was forgotten in step 10

--- webapp/Component.js
+++ webapp/Component.js
@@ -2,9 +2,7 @@ sap.ui.define(
 	[
 		'sap/ui/core/UIComponent',
 		'sap/ui/model/json/JSONModel',
-		'sap/ui/model/resource/ResourceModel',
 	],
-	function (UIComponent, JSONModel, ResourceModel) {
 		'use strict';
 
 		return UIComponent.extend('sap.ui.demo.walkthrough.Component', {
@@ -24,12 +22,6 @@ sap.ui.define(
 				var oModel = new JSONModel(oData);
 				this.setModel(oModel);
 
-				var i18nModel = new ResourceModel({
-					bundleName: 'sap.ui.demo.walkthrough.i18n.i18n',
-					supportedLocales: [''],
-					fallbackLocale: '',
-				});
-				this.setModel(i18nModel, 'i18n');
 			},
 		});
 	}
