b1d91d05ed24f0a5fb26a3af1621ef8cf39fdde7
Rogier Spieker <rogier@konfirm.net>
2020-12-12 17:00:12 +0100

    Added ProductRating control

---
+++ webapp/control/ProductRating.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+	'sap/ui/core/Control'
+], function (Control) {
+	'use strict';
+
+	return Control.extend('sap.ui.demo.walkthrough.control.ProductRating', {
+		metadata: {
+		},
+		init: function () {
+		},
+		renderer: function (oRM, oControl) {
+		}
+	});
+});
