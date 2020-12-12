67728037f2b7ace03dd290f6dbe0e5b6f7092591
Rogier Spieker <rogier@konfirm.net>
2020-12-12 15:47:47 +0100

    Proper currency formatting

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,6 +1,14 @@
 sap.ui.define([], function () {
 	'use strict';
 	return {
+		properCurrency: function (value, currency) {
+			const formatter = new Intl.NumberFormat('nl', {
+				style: 'currency',
+				currency,
+			});
+
+			return formatter.format(value);
+		},
 		statusSimple: function (status) {
 			const bundle = this.getView().getModel('i18n').getResourceBundle();
 			const invoiceStatus = `invoiceStatus${status}`;
