2461e48a7370f5c4cacbafb59cf8776c8abc9d55
Rogier Spieker <rogier@konfirm.net>
2020-12-13 11:15:02 +0100

    Removed hardcoded expectations on key names
    Broken down every subsequent nested object to learn the types (or lack thereof)

--- webapp/model/formatter.ts
+++ webapp/model/formatter.ts
@@ -1,18 +1,18 @@
-sap.ui.define([], () => {
-	return {
-		statusText(status) {
-			const bundle = this.getView().getModel('i18n').getResourceBundle();
-
-			switch (status) {
-				case 'A':
-					return bundle.getText('invoiceStatusA');
-				case 'B':
-					return bundle.getText('invoiceStatusB');
-				case 'C':
-					return bundle.getText('invoiceStatusC');
-				default:
-					return status;
-			}
-		}
+namespace sap.base.i18n {
+	export type ResourceBundle = {
+		getText(key: string, args?: any[], ignoreKeyFallback?: boolean): string;
+		hasText(key: string): boolean;
 	};
-});
+}
+
+sap.ui.define([], () => ({
+	statusText(status: string): string {
+		const view: sap.ui.core.mvc.View = this.getView();
+		const model: sap.ui.model.Model = view.getModel('i18n');
+		const resource: sap.ui.model.resource.ResourceModel = model as sap.ui.model.resource.ResourceModel;
+		const bundle: sap.base.i18n.ResourceBundle = resource.getResourceBundle();
+		const invoiceStatusKey: string = `invoiceStatus${status}`;
+
+		return bundle.getText(bundle.hasText(invoiceStatusKey) ? invoiceStatusKey : status);
+	}
+}));
